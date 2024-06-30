fc8f2db32dbf0763b4e51c2e0cf848f1de47413d
unknown <2200016073@webmail.uad.ac.id>
2024-06-30 09:54:33 +0700

    Prak7

---
+++ lib/inbox.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'mail_card_preview.dart';
+import 'model/email_store.dart';
+
+class InboxPage extends StatelessWidget {
+  // ignore: use_super_parameters
+  const InboxPage({required this.destination, Key? key}) : super(key: key);
+
+  final String destination;
+
+  @override
+  Widget build(BuildContext context) {
+    const horizontalPadding = 4.0;
+
+    return Consumer<EmailStore>(
+      builder: (context, model, child) {
+        return SafeArea(
+          bottom: false,
+          child: model.emails[destination]!.isEmpty
+              ? Center(
+                  child: Text(
+                    'Empty in ${destination.toLowerCase()}',
+                  ),
+                )
+              : ListView.separated(
+                  itemCount: model.emails[destination]!.length,
+                  padding: const EdgeInsetsDirectional.only(
+                    start: horizontalPadding,
+                    end: horizontalPadding,
+                    bottom: kToolbarHeight,
+                  ),
+                  primary: false,
+                  separatorBuilder: (context, index) =>
+                      const SizedBox(height: 4),
+                  itemBuilder: (context, index) {
+                    return MailPreviewCard(
+                      id: index,
+                      email: model.emails[destination]!.elementAt(index),
+                      onDelete: () => model.deleteEmail(destination, index),
+                      onStar: () => model.starEmail(destination, index),
+                    );
+                  },
+                ),
+        );
+      },
+    );
+  }
+}
